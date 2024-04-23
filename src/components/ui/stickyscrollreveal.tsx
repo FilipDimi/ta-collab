5e7e48e9c1f217be9f311f9f46a1f09aa74a68f8
dimitr36 <dimitr36@msu.edu>
2024-04-23 00:12:18 -0400

    Landing page started

---
+++ src/components/ui/stickyscrollreveal.tsx
@@ -0,0 +1,54 @@
+"use client";
+import React from "react";
+import { StickyScroll } from "../aceternity/sticky-scroll-reveal";
+import Image from "next/image";
+
+const content = [
+  {
+    title: "Contact Your TA",
+    description:
+      "Work together in real time with your team, clients, and stakeholders. Collaborate on documents, share ideas, and make decisions quickly. With our platform, you can streamline your workflow and increase productivity.",
+    content: (
+      <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] flex items-center justify-center text-white">
+        Contact Your TA
+      </div>
+    ),
+  },
+  {
+    title: "Check TA Hours",
+    description:
+      "See changes as they happen. With our platform, you can track every modification in real time. No more confusion about the latest version of your project. Say goodbye to the chaos of version control and embrace the simplicity of real-time updates.",
+    content: (
+      <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] flex items-center justify-center text-white">
+        Find Valuable Resources
+      </div>
+    ),
+  },
+  {
+    title: "Forum",
+    description:
+      "Experience real-time updates and never stress about version control again. Our platform ensures that you're always working on the most recent version of your project, eliminating the need for constant manual updates. Stay in the loop, keep your team aligned, and maintain the flow of your work without any interruptions.",
+    content: (
+      <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--orange-500),var(--yellow-500))] flex items-center justify-center text-white">
+        Version control
+      </div>
+    ),
+  },
+  {
+    title: "Find Valuable Resources",
+    description:
+      "Experience real-time updates and never stress about version control again. Our platform ensures that you're always working on the most recent version of your project, eliminating the need for constant manual updates. Stay in the loop, keep your team aligned, and maintain the flow of your work without any interruptions.",
+    content: (
+      <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] flex items-center justify-center text-white">
+        Find Valuable Resources
+      </div>
+    ),
+  },
+];
+export function StickyScrollReveal() {
+  return (
+    <div className="">
+      <StickyScroll content={content} />
+    </div>
+  );
+}
